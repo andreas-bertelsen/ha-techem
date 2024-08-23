5aa9d88a70df6647acb1801b30e3b8f63e4e20a8
Andreas Bertelsen <andreas.k.bertelsen@gmail.com>
2024-08-23 12:54:44 +0200

    fix: Update graphql format (closes #1)

--- scripts/python/techem.py
+++ scripts/python/techem.py
@@ -4,20 +4,20 @@ import datetime
 import json
 
 # Get date (YYYY-MM-DDThh:mm:ss.000Z) n days ago
-def get_time_as_string(n: int):
+def get_time_as_string(n: int) -> str:
     today = datetime.datetime.now()
     date = today - datetime.timedelta(days=n)
 
-    return f"{date.year}-{date.month:02d}-{date.day:02d}T{date.hour:02d}:{date.minute:02d}:{date.second:02d}.000Z"
+    return f"{date.year}-{date.month:02d}-{date.day:02d}"
 
 # Get date (YYYY-MM-DDThh:mm:ss.000Z) as tuple with first day of year and yesterday
-def get_time_as_string_year():
+def get_time_as_string_year() -> tuple[str, str]:
     today = datetime.datetime.now()
     yesterday = today - datetime.timedelta(1)
-    stop = f"{yesterday.year}-{yesterday.month:02d}-{yesterday.day:02d}T{yesterday.hour:02d}:{yesterday.minute:02d}:{yesterday.second:02d}.000Z"
+    stop = f"{yesterday.year}-{yesterday.month:02d}-{yesterday.day:02d}"
 
     firstDay = datetime.datetime(today.year, 1, 1)
-    start = f"{firstDay.year}-{firstDay.month:02d}-{firstDay.day:02d}T{firstDay.hour:02d}:{firstDay.minute:02d}:{firstDay.second:02d}.000Z"
+    start = f"{firstDay.year}-{firstDay.month:02d}-{firstDay.day:02d}"
 
     return start, stop
 
@@ -34,7 +34,7 @@ def get_token(techemEmail: str, techemPassword: str):
 
     return requests.post("https://techemadmin.no/graphql", headers=token_headers, data=token_body, timeout=10.0).json()["data"]["tokenAuth"]["token"]
 
-def get_data(tenantID: int, yearly: bool):
+def get_data(tenantID: int, yearly: bool) -> str:
     token = get_token(sys.argv[1], sys.argv[2])
 
     starttime, endtime = "", ""
@@ -47,8 +47,8 @@ def get_data(tenantID: int, yearly: bool):
         starttime = get_time_as_string(9)
         endtime = get_time_as_string(2)
 
-    body = "{\"operationName\":\"getDashboardPage\",\"variables\":{\"tenancyId\":" + str(tenantID) + ",\"periodBegin\":\"" + starttime + "\",\"periodEnd\":\"" + endtime + "\",\"compareWith\":\"last_year\"},\"query\":\"query getDashboardPage($tenancyId: Int!, $periodBegin: String, $periodEnd: String, $compareWith: String!) {\\n  dashboard(\\n   tenancyId: $tenancyId\\n    periodBegin: $periodBegin\\n    periodEnd: $periodEnd\\n    compareWith: $compareWith\\n  ) {\\n    consumptions {\\n      value\\n      kind\\n      comparePercent\\n      __typename\\n    }\\n    climateAverages {\\n      value\\n      valueCompare\\n      kind\\n      __typename\\n    }\\n    __typename\\n  }\\n}\"}"
-    
+    body = "{\"query\":\"\\n      query DashboardData($tenancyId: Int!, $periodBegin: String, $periodEnd: String, $compareWith: String!) {\\n        dashboard(tenancyId: $tenancyId, periodBegin: $periodBegin, periodEnd: $periodEnd, compareWith: $compareWith) {\\n          consumptions {\\n            value\\n            comparisonValue\\n            kind\\n            comparePercent\\n          }\\n          climateAverages {\\n            value\\n            valueCompare\\n            kind\\n          }\\n        }\\n      }\\n    \",\"variables\":{\"tenancyId\":" + str(TECHEMTENANTID) + ",\"periodBegin\":\"" + starttime + "\",\"periodEnd\":\"" + endtime + "\",\"compareWith\":\"last_period\"},\"operationName\":\"DashboardData\"}"
+
     headers = {
         "Accept": "*/*",
         "Accept-Encoding": "gzip, deflate, br",
@@ -60,9 +60,6 @@ def get_data(tenantID: int, yearly: bool):
     }
 
     response = requests.post("https://techemadmin.no/graphql", headers=headers, data=body, timeout=10.0).json()["data"]["dashboard"]["consumptions"]
-    
-    for entry in response:
-        del entry["__typename"]
 
     return json.dumps(response)
 
